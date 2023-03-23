b047eb59205d211f0149a6d16a6c12deead195e6
Ricardo Gonçalves <ricardo@conceitho.com>
2023-03-23 09:55:18 -0300

    feat: compartilha evento através do contexto

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,15 @@
+import { useUsuarioLogado } from "../../shared/hooks";
+
 export const HomePage = () => {
+    const { nome, onLogout } = useUsuarioLogado();
+
     return (
         <>
             <h1>Página Home</h1>
+            <br />
+            <h2>Usuário Logado: {nome}</h2>
+
+            <button type="button" onClick={onLogout}>Logout</button>
         </>
     );
 }
--- src/shared/contexts/UsuarioLogado.tsx
+++ src/shared/contexts/UsuarioLogado.tsx
@@ -1,8 +1,9 @@
-import React, { createContext } from "react";
+import React, { createContext, useCallback } from "react";
 
 interface IUsuarioLogadoContextData {
     nome: string;
     emailLogado?: string;
+    onLogout: () => void;
 }
 
 interface IProps {
@@ -12,8 +13,12 @@ interface IProps {
 export const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData>({} as IUsuarioLogadoContextData);
 
 export const UsuarioLogadoProvider: React.FC<IProps> = ({ children }) => {
+    const handleLogout = useCallback(() => {        
+        console.log('Logout realizado');
+    }, [])
+
     return (
-        <UsuarioLogadoContext.Provider value={{ nome: 'Ricardo', emailLogado: 'ricardo@goncalves.biz' }}>
+        <UsuarioLogadoContext.Provider value={{ nome: 'Ricardo', emailLogado: 'ricardo@goncalves.biz', onLogout: handleLogout }}>
             {children}
         </UsuarioLogadoContext.Provider>
     );
