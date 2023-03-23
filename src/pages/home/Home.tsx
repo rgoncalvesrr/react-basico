8ed94ba64c26dedc9b4bb82d32df9cbd1a2487d9
Ricardo Gonçalves <ricardo@conceitho.com>
2023-03-23 11:03:12 -0300

    feat: adiciona lista na página inicial

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,50 @@
-import { useUsuarioLogado } from "../../shared/hooks";
+import { useCallback, useState } from "react";
 
 export const HomePage = () => {
-    const { nome, onLogout } = useUsuarioLogado();
+    const [lista, setLista] = useState<string[]>(['Joana', 'Jobson', 'Tadeu', 'Ernesto']);
+
+    const handlerInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+        if (e.key !== 'Enter')
+            return;
+
+        if (e.currentTarget.value.trim().length === 0) return;
+
+        const value = e.currentTarget.value;
+        e.currentTarget.value = '';
+
+        // Esta é a forma correta de incrementar uma lista em React
+        //
+        // Não deve ser usado setList([...lista, e.currentTarget.value]) porque se houver nova chamada
+        // subsequente da função setList dentro do mesmo evento o primeiro item adicionado é perdido.
+        //
+        // Exemplo:
+        //  
+        //  setList([...lista, 'Josefina']);
+        //
+        //  setList([...lista, 'Romeu']);
+        //
+        // Nesse exemplo, o  valor 'Josefina' será pedido, porque ambas as chamadas adicionam o novo elemento
+        // a partir da lista original
+        setLista((oldLista) => {
+
+            if (oldLista.includes(value))
+                return oldLista;
+
+            return [...oldLista, value];
+        });
+    }, []);
 
     return (
         <>
-            <h1>Página Home</h1>
-            <br />
-            <h2>Usuário Logado: {nome}</h2>
-
-            <button type="button" onClick={onLogout}>Logout</button>
+            <p>Lista</p>
+            <input type="text" onKeyDown={handlerInputKeyDown} />
+            <ul>
+                {
+                    lista.map((value, index) => {
+                        return <li key={index}>{value}</li>
+                    })
+                }
+            </ul>
         </>
     );
 }
